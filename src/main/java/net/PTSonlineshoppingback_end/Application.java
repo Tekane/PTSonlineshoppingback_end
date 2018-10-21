db3ff1cedec9c5068a420b889d658c2e46bba426
tekane <tekane@Kgodisho>
2018-10-21 21:31:26 +0200

    First commit for onlineshopping back-end

--- src/main/java/net/PTSonlineshoppingback_end/Application.java
+++ src/main/java/net/PTSonlineshoppingback_end/Application.java
@@ -1,4 +1,4 @@
-package com.PTSonlineshoppingback_end;
+package net.PTSonlineshoppingback_end;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
